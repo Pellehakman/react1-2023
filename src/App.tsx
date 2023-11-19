b34736964633a8d4cf86d5e7d0bfacbea17e947c
Pellehakman <83712876+Pellehakman@users.noreply.github.com>
2023-11-19 01:28:43 +0100

    e

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import SiteNav from "./components/siteNav";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("pelle");
-
-  const urls = ["url1", "url2", "url3"];
-  function work() {
-   
-
-   
-  
-   
-    function printNumbers(n: number) {
-      for (let i = 1; i <= n; i++) {
-          console.log(i);
-      }
-  }
-  
-  printNumbers(5);  // Prints numbers 1 to 5
-  printNumbers(10); // Prints numbers 1 to 10
-  }
-
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card jobb">
-        <button onClick={work}>click me</button>
-        <button onClick={() => setCount((count) => count + 3 * 223)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>{name}</p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SiteNav />
     </>
   );
 }
 
-
-
 export default App;
-
-
-
---
+++ src/components/siteNav.tsx
@@ -0,0 +1,10 @@
+import "./siteNav.css";
+function SiteNav() {
+  return (
+    <nav className="nav-container">
+      <h1>siteNav</h1>
+    </nav>
+  );
+}
+
+export default SiteNav;
