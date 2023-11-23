8087502507c2a757fa0c575a58c0bd82f3952c54
Pellehakman <83712876+Pellehakman@users.noreply.github.com>
2023-11-23 16:27:42 +0100

    s

---
+++ src/components/Nav/siteNav/siteNav.md
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./siteNav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { motion } from "framer-motion";
+
+const SiteNav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <div className={`fixed top-0 left-0 ${menuOpen ? "w-full" : "w-1/2"}`}>
+      <div
+        className="bg-gray-800 p-4 text-white cursor-pointer"
+        onClick={toggleMenu}
+      >
+        Menu
+      </div>
+      {menuOpen && (
+        <div className="fixed top-0 left-1/2 transform -translate-x-1/2 w-1/2 h-full bg-white shadow-md">
+          {/* Your menu content goes here */}
+          <div
+            className="p-4 bg-gray-800 text-white cursor-pointer"
+            onClick={toggleMenu}
+          >
+            Close
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SiteNav;
+ 
+       <FontAwesomeIcon className="p-4 absolute" icon={faBars} onClick={toggleMenu} />
--- src/components/Nav/siteNav/siteNav.tsx
+++ src/components/Nav/siteNav/siteNav.tsx
@@ -1,34 +1,43 @@
 import React, { useState } from "react";
-import "./siteNav.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-function SiteNav() {
-  const [activeMenu, setActiveMenu] = useState(false);
-  const variants = {
-    open: { opacity: 1, height: "100vh", overflowY: "scroll" },
-    closed: { opacity: 0, height: 0, overflowY: "hidden" },
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: "-100%" },
+};
+
+const SiteNav = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
   };
-  const toggleMenu = () => setActiveMenu((prevActiveMenu) => !prevActiveMenu);
 
   return (
-    <>
-      <FontAwesomeIcon className="p-4 absolute" icon={faBars} onClick={toggleMenu} />
-
-      {activeMenu && (
-        <motion.div
-          className="bg-slate-600"
-          initial="closed"
-          animate={activeMenu ? "open" : "closed"}
-          variants={variants}
-        >
-          <p>hej</p>
-          {/* Add more content as needed */}
-        </motion.div>
-      )}
-    </>
+    <div>
+      <FontAwesomeIcon
+        icon={faBars}
+        className={`p-4 text-white cursor-pointer ${
+          menuOpen ? "hidden" : "block"
+        }`}
+        onClick={toggleMenu}
+      />
+      <motion.nav
+        className={`fixed top-0 left-0 w-2/3 h-full bg-stone-800 shadow-md`}
+        animate={menuOpen ? "open" : "closed"}
+        variants={variants}
+      >
+        <FontAwesomeIcon
+          icon={faTimes}
+          className="p-4 text-white cursor-pointer"
+          onClick={toggleMenu}
+        />
+        <Items />
+      </motion.nav>
+    </div>
   );
-}
+};
 
 export default SiteNav;
