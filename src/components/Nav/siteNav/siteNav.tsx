fbb57e47b7f61b2c6598436a01420a440870335d
Pellehakman <83712876+Pellehakman@users.noreply.github.com>
2023-11-24 14:36:24 +0100

    s

--- src/components/Nav/siteNav/siteNav.tsx
+++ src/components/Nav/siteNav/siteNav.tsx
@@ -2,13 +2,37 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-
-const variants = {
-  open: { opacity: 1, x: 0 },
-  closed: { opacity: 0, x: "-100%" },
-};
+import NavItems from "./NavItems/NavItems";
 
 const SiteNav = () => {
+  const variants = {
+    open: { opacity: 1, x: 0 },
+    closed: { opacity: 0, x: "-100%" },
+  };
+  const variants2 = {
+    open: {
+      transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+    },
+    closed: {
+      transition: { staggerChildren: 0.05, staggerDirection: -1 },
+    },
+  };
+  const variants3 = {
+    open: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        y: { stiffness: 1000, velocity: -100 },
+      },
+    },
+    closed: {
+      y: 50,
+      opacity: 0,
+      transition: {
+        y: { stiffness: 1000 },
+      },
+    },
+  };
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
@@ -34,7 +58,16 @@ const SiteNav = () => {
           className="p-4 text-white cursor-pointer"
           onClick={toggleMenu}
         />
-        {/* <Items /> */}
+        <motion.ul className="p-4" variants={variants2}>
+          <motion.li
+            className="p-4  rounded-sm bg-black shadow-md"
+            variants={variants3}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            menuItem
+          </motion.li>
+        </motion.ul>
       </motion.nav>
     </div>
   );
